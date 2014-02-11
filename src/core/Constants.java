38516d6af12170a70992016a06ccd8871ec7016f
wrh1986 <dndx61003127@gmail.com>
2014-02-12 06:45:22 +0800

    test

--- src/core/Constants.java
+++ src/core/Constants.java
@@ -10,6 +10,6 @@ public class Constants {
   public static final String TEMPLATE_PATH_PRE = ROOT_PATH + "template/predefined";
   public static final String TEMPLATE_PATH_TEMP = ROOT_PATH + "template/temp";
 
-  public static final String PROPERTY_SPLIT = ";";
+  public static final String PROPERTY_SPLIT = ";"; 
   public static final String KEY_VALUE_SPLIT = "=";
 }
