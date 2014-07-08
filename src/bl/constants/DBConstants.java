5a328b5352659b9ffc749d73fd5c13be8ba491de
wrh1986 <dndx61003127@gmail.com>
2014-07-08 23:49:30 +0800

    Data input function

---
+++ src/bl/constants/DBConstants.java
@@ -0,0 +1,15 @@
+package bl.constants;
+
+/**
+ * Created by wangronghua on 14-7-4.
+ */
+public class DBConstants {
+
+
+    public static final String DB_FIELD_STUDYID="studyId";
+    public static final String DB_FIELD_VIEWID="viewId";
+    public static final String DB_FIELD_PARTICIPANTID="participantId";
+    public static final String DB_FIELD_CREATETIME="createTime";
+    public static final String DB_FIELD_ID="_id";
+
+}
